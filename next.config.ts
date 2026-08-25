28aba6ffb5661c48cd845f5d573dbebb16387c2e
Mohammad Bagher Zolfaghari <81497912+mbz1372@users.noreply.github.com>
2026-08-25 15:57:41 +0330

    chore: initialize SmartOne platform (next.config.ts)

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  experimental: { typedEnv: true },
+};
+
+export default nextConfig;
